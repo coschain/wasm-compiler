6e776c329a5cc2c702752534e97d08d4ecadf7e3
daishuo <daishuo@gmail.com>
2018-12-17 16:02:01 +0800

    fix bugs of print functions

--- contract/cosiolib/print.hpp
+++ contract/cosiolib/print.hpp
@@ -27,20 +27,12 @@ namespace cosio {
         prints_l(&c, 1);
     }
     
-    inline void print(int n) {
-        ::print_int(n);
-    }
-    
     inline void print(int32_t n) {
         ::print_int(n);
     }
     
     inline void print(int64_t n) {
         ::print_int(n);
-    }
-    
-    inline void print(unsigned int n) {
-        ::print_uint(n);
     }
     
     inline void print(uint32_t n) {
@@ -57,7 +49,7 @@ namespace cosio {
     
     template <typename T>
     inline void print(T&& obj) {
-        t.print();
+        obj.print();
     }
     
     inline void print_f(const char *s) {
@@ -79,17 +71,8 @@ namespace cosio {
     
     template <typename Arg, typename...Args>
     inline void print(Arg&& val, Args&&...rest) {
-        print(std::forward<Arg>(a));
+        print(std::forward<Arg>(val));
         print(std::forward<Args>(rest)...);
-    }
-    
-    class iostream {};
-    static iostream cout;
-    
-    template <typename T>
-    inline iostream& operator << (iostream& out, const T& obj) {
-        print(obj);
-        return out;
     }
 
 }
