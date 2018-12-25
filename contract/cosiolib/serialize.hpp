7eda0b67826c4a135f2b0baf3d027a32ff9e58a6
daishuo <daishuo@gmail.com>
2018-12-25 18:44:02 +0800

    rename serialization macro

--- contract/cosiolib/serialize.hpp
+++ contract/cosiolib/serialize.hpp
@@ -6,32 +6,32 @@
 #include "varint.hpp"
 #include "assert.hpp"
 
-#define COSLIB_REFLECT_MEMBER_OP( r, OP, elem ) \
+#define COSIO_REFLECT_MEMBER_OP( r, OP, elem ) \
   OP t.elem 
 
-#define COSLIB_SERIALIZE( TYPE,  MEMBERS ) \
+#define COSIO_SERIALIZE( TYPE,  MEMBERS ) \
  template<typename DataStream> \
  friend DataStream& operator << ( DataStream& ds, const TYPE& t ){ \
     ds << cosio::unsigned_int( BOOST_PP_SEQ_SIZE( MEMBERS ) ); \
-    return ds BOOST_PP_SEQ_FOR_EACH( COSLIB_REFLECT_MEMBER_OP, <<, MEMBERS );\
+    return ds BOOST_PP_SEQ_FOR_EACH( COSIO_REFLECT_MEMBER_OP, <<, MEMBERS );\
  }\
  template<typename DataStream> \
  friend DataStream& operator >> ( DataStream& ds, TYPE& t ){ \
     cosio::unsigned_int s; \
     ds >> s; \
     cosio::cosio_assert( BOOST_PP_SEQ_SIZE( MEMBERS ) == s.value, "unpacking " BOOST_PP_STRINGIZE(TYPE) ": field count mismatched."); \
-    return ds BOOST_PP_SEQ_FOR_EACH( COSLIB_REFLECT_MEMBER_OP, >>, MEMBERS );\
+    return ds BOOST_PP_SEQ_FOR_EACH( COSIO_REFLECT_MEMBER_OP, >>, MEMBERS );\
  } \
 public:\
 static const char* _cosio_type_name() { return BOOST_PP_STRINGIZE(TYPE); }
 
 
-#define COSLIB_SERIALIZE_DERIVED( TYPE, BASE, MEMBERS ) \
+#define COSIO_SERIALIZE_DERIVED( TYPE, BASE, MEMBERS ) \
  template<typename DataStream> \
  friend DataStream& operator << ( DataStream& ds, const TYPE& t ){ \
     ds << cosio::unsigned_int( BOOST_PP_SEQ_SIZE( MEMBERS ) + 1 ); \
     ds << static_cast<const BASE&>(t); \
-    return ds BOOST_PP_SEQ_FOR_EACH( COSLIB_REFLECT_MEMBER_OP, <<, MEMBERS );\
+    return ds BOOST_PP_SEQ_FOR_EACH( COSIO_REFLECT_MEMBER_OP, <<, MEMBERS );\
  }\
  template<typename DataStream> \
  friend DataStream& operator >> ( DataStream& ds, TYPE& t ){ \
@@ -39,7 +39,7 @@ static const char* _cosio_type_name() { return BOOST_PP_STRINGIZE(TYPE); }
     ds >> s; \
     cosio::cosio_assert( BOOST_PP_SEQ_SIZE( MEMBERS ) + 1 == s.value, "unpacking " BOOST_PP_STRINGIZE(TYPE) ":field count mismatched."); \
     ds >> static_cast<BASE&>(t); \
-    return ds BOOST_PP_SEQ_FOR_EACH( COSLIB_REFLECT_MEMBER_OP, >>, MEMBERS );\
+    return ds BOOST_PP_SEQ_FOR_EACH( COSIO_REFLECT_MEMBER_OP, >>, MEMBERS );\
  } \
 public:\
 static const char* _cosio_type_name() { return BOOST_PP_STRINGIZE(TYPE); }
