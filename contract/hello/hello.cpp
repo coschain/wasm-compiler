ca389ed62a2b16d884f5a618f66a36751165f8de
daishuo <daishuo@gmail.com>
2018-12-27 11:40:26 +0800

    singleton usage demo added in hello contract

--- contract/hello/hello.cpp
+++ contract/hello/hello.cpp
@@ -13,12 +13,27 @@ struct greeting {
     COSIO_SERIALIZE(greeting, (name)(count)(last_seen))
 };
 
+// the singleton record type, which must be derived from cosio::singleton_record.
+struct stats : public cosio::singleton_record {
+    stats(): users(0), visits(0) {}
+
+    uint32_t users;
+    uint32_t visits;
+    
+    // record type must support serialization.
+    COSIO_SERIALIZE_DERIVED(stats, cosio::singleton_record, (users)(visits))
+};
+
 // the contract class
 class hello : public cosio::contract {
 public:
     using cosio::contract::contract;
 
     void hi( cosio::account_name user ) {
+        
+        // load the global counter
+        counter.get_or_create();
+        
         // check if we have greeted this guy before
         if(!table_greetings.has(user)) {
             // first time meeting
@@ -27,16 +42,24 @@ public:
                 r.count = 1;
                 r.last_seen = cosio::current_timestamp();
             });
+            counter.update([&](stats& s) {
+                s.users++;
+                s.visits++;
+            });
         } else {
             // increase the greeting counter
             table_greetings.update(user, [&](greeting& r) {
                 r.count++;
                 r.last_seen = cosio::current_timestamp();
             });
+            counter.update([&](stats& s) {
+                s.visits++;
+            });
         }
         // print something
         auto r = table_greetings.get(user);
-        cosio::print("Hello %, we have met % times.", user, r.count);
+        auto s = counter.get();
+        cosio::print("Hello %, we have met % times. I have greeted % persons, % greetings in total.", user, r.count, s.users, s.visits);
     }
 private:
     //
@@ -57,6 +80,12 @@ private:
     //
     COSIO_DEFINE_NAMED_TABLE( table_hello, "hello", greeting, (name) );
     
+    //
+    // define a class data member named "counter" representing a singleton table which,
+    // - has name "global_counters"
+    // - has a record type of stats
+    //
+    COSIO_DEFINE_NAMED_SINGLETON( counter, "global_counters", stats );
 };
 
 // declare that this contract is named "hello", and has 1 method named "hi".
