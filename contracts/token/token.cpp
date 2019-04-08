56c792828241da8955297e035c3a85b6ae3eaa92
yykingking <yykingking@gmail.com>
2019-04-08 22:04:47 +0800

    modify token sample

--- contracts/token/token.cpp
+++ contracts/token/token.cpp
@@ -3,31 +3,26 @@
 #include <cosiolib/system.hpp>
 
 using namespace std;
-/*
-struct TokenInterface {
-    virtual uint32_t totalSupply() = 0;
-    virtual uint32_t banlanceOf(cosio::name tokenOwner) = 0;
-    virtual void transfer(cosio::name from,cosio::name to, uint32_t amount) = 0;
-};
-*/
+
 
 struct balance {
     cosio::name tokenOwner;
-    uint32_t amount;
+    uint64_t amount;
     COSIO_SERIALIZE(balance, (tokenOwner)(amount))
 };
 
 struct stat : public cosio::singleton_record {
     string name;
     string symbol;
-    uint32_t total_supply;
-    COSIO_SERIALIZE_DERIVED(stat, cosio::singleton_record, (name)(symbol)(total_supply))
+    uint64_t total_supply;
+    uint32_t decimals;
+    COSIO_SERIALIZE_DERIVED(stat, cosio::singleton_record, (name)(symbol)(total_supply)(decimals))
 };
 
 struct cosToken : public cosio::contract {
     using cosio::contract::contract;
 
-    void create(string name,string symbol, uint32_t total_supply) {
+    void create(string name,string symbol, uint64_t total_supply, uint32_t decimals) {
         cosio::require_auth(get_name().account());
 
         stats.get_or_create();
@@ -35,11 +30,11 @@ struct cosToken : public cosio::contract {
                 s.name = name;
                 s.symbol = symbol;
                 s.total_supply = total_supply;
+                s.decimals = decimals;
                 });
 
         // give init supply to contract owner
         auto owner = get_name();
-        cosio::print_f("create call 1 : name %, symbol %, total_supply %, owner % \n", name,symbol,total_supply,owner.account());
         balances.insert([&](balance& b){
             auto account_name = owner.account();
             b.tokenOwner.set_string(account_name);
@@ -47,10 +42,10 @@ struct cosToken : public cosio::contract {
         });
 
         auto b = balances.get(owner.account());
-        cosio::print_f("create call 2 : user % have % amount. \n", b.tokenOwner.string(), b.amount);
+        cosio::print_f("user % have % amount. \n", b.tokenOwner.string(), b.amount);
     }
 
-    void transfer(cosio::name from,cosio::name to, uint32_t amount) {
+    void transfer(cosio::name from,cosio::name to, uint64_t amount) {
         cosio::require_auth(from);
 
         cosio::cosio_assert(balances.has(from), std::string("balance not exist:") + from.string());
